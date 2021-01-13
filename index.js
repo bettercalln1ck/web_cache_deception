beb786bf7855115a0656b3128bf8d582c12d525e
nikolai6303 <iit2018152@iiita.ac.in>
2021-01-13 16:20:35 +0530

    nginx filw yet to add

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/login/", (req, res) => {
+  const { name, password } = req.body;
+
+  if (name === "admin" && password === "admin") {
+    res.render("success", {
+      username: name,
+    });
+  } else {
+    res.render("failure");
+  }
+});
+
+app.post("/login/:param", (req, res) => {
+	  const { name, password } = req.body;
+
+	  if (name === "admin" && password === "admin") {
+		      res.render("success", {
+			            username: name,
+			          });
+		    } else {
+			        res.render("failure");
+			      }
+});
+
+app.listen(3000, () => {
+  console.log("server started on port 3000");
+});
