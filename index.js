1bacf53743fdfa4e9ad89c323e6fff36b6d6223c
nikolai6303 <iit2018152@iiita.ac.in>
2021-01-17 14:46:10 +0530

    vulnerability successfully duplicated

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
+app.set('etag', false);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+
+
+
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -15,7 +21,11 @@ app.post("/login/", (req, res) => {
   const { name, password } = req.body;
 
   if (name === "admin" && password === "admin") {
-    res.render("success", {
+	  res.set( {
+      "Set-Cookie": "token=encryptedstring; HttpOnly",
+      "Access-Control-Allow-Credentials": "true"
+    })
+    .render("success", {
       username: name,
     });
   } else {
@@ -23,7 +33,7 @@ app.post("/login/", (req, res) => {
   }
 });
 
-app.post("/login/:param", (req, res) => {
+/*app.post("/login/:param", (req, res) => {
 	  const { name, password } = req.body;
 
 	  if (name === "admin" && password === "admin") {
@@ -33,6 +43,16 @@ app.post("/login/:param", (req, res) => {
 		    } else {
 			        res.render("failure");
 			      }
+});*/
+
+app.get("/private", (req, res) => {
+  if (!req.cookies.token) return res.status(401).send();
+  res.status(200).json({ secret: "Ginger ale is a specific Root Beer" });
+});
+
+app.get("/private/:param", (req, res) => {
+  if (!req.cookies.token) return res.status(401).send();
+  res.status(200).json({ secret: "Ginger ale is a specific Root Beer" });
 });
 
 app.listen(3000, () => {
